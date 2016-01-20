f309424e83d5619a9f7ce15103f8f5033054f26d
hhhome <caihaohuan@frogshealth.com>
2016-01-20 15:01:11 +0800

    add jshint

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,19 @@
  */
 var gulp = require("gulp");
 var nodemon = require("gulp-nodemon");
+var jshint = require("gulp-jshint");
 
-gulp.task("dev", function(){
+
+var paths = {
+    scripts: ['public/javascripts/*.js','routes/*.js']
+};
+
+gulp.task('lint', function () {
+  return gulp.src(paths.scripts)
+      .pipe(jshint())
+      .pipe(jshint.reporter('default'));
+});
+
+gulp.task("dev",['lint'], function(){
   nodemon('script:./bin/www');
 });
